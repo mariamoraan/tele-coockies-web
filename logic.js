ecb34592ddfc7cbf9069f270990ad1b1d775be84
mia-moran <mariamoranluaces@gmail.com>
2021-10-09 18:22:18 +0200

    footer added, nav fixed

--- logic.js
+++ logic.js
@@ -1,5 +1,6 @@
 var counter=1;
-var max_counter = 3;
+var max_counter = 4;
+document.documentElement.style.setProperty('--slides', max_counter);
 setInterval(function(){
     document.getElementById('radio-' + counter).checked = true;
     counter++;
